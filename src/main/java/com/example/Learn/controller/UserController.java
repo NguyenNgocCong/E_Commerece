640ea82a5a396dfaa5be3f9694046891cdf23852
NguyenNgocCong <86562348+NguyenNgocCong@users.noreply.github.com>
2023-06-06 10:17:02 +0700

    1

---
+++ src/main/java/com/example/Learn/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.Learn.controller;
+
+import com.example.Learn.model.Users;
+import com.example.Learn.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    Users newUser(@RequestBody Users newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<Users> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+
+
+}
